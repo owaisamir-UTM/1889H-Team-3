69b14b0c63002c4b9ecba3e792e22a3d6c8638b4
owaisamir-UTM <owais.amir@mail.utoronto.ca>
2026-03-25 23:56:18 -0400

    Created main code file

---
+++ BTC1889H-Team_3_Code.R
@@ -0,0 +1,11 @@
+############################################################
+# Deep Learning in Health (BTC1889H)
+# Final Project: Deep Learning Models for Text Data
+#
+# Team Members:
+# - Owais Amir
+# - Lamees Anjum
+# - Anna Fan
+# - Anthony Pede
+# - Ann Zhang
+############################################################
